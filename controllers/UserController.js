029110cb8201fa6582c4c2b97c52a27a59faeb45
hanmok <dmammmm@naver.com>
2022-12-20 22:57:33 +0900

    fix: return one user instead of user's' when log in

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,7 @@ exports.getUser = async (req, res, next) => {
 	try {
 		let {username, password} = req.body;
 		const [user, _] = await User.login(username, password);
-		res.status(200).json({user: user})
+		res.status(200).json({user: user[0]})
 	} catch (error) { 
 		console.log(error);
 		next(error);
